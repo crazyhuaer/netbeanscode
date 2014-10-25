4bac8c13ddd7ab4f671a7d88b2c23ca14be86803
crazyhuaer <h20549304@gmail.com>
2014-10-25 18:57:34 +0800

    add uthash example test

---
+++ C/study/uthash/hashtable.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   hashtable.h
+ * Author: topseten
+ *
+ * Created on October 25, 2014, 9:22 PM
+ */
+
+#ifndef HASHTABLE_H
+#define	HASHTABLE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* HASHTABLE_H */
+
--- C/study/uthash/main.c
+++ C/study/uthash/main.c
@@ -2,34 +2,187 @@
  * File:   main.c
  * Author: topseten
  *
- * Created on October 24, 2014, 11:30 AM
+ * Created on October 25, 2014, 7:01 PM
  */
-
+//////////////////////////////////////////////////////////////////////////////
+// header file.
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "uthash.h"
 
-typedef struct example_user_t {
+//////////////////////////////////////////////////////////////////////////////
+// struct define
+typedef struct _example {
     int id;
-    int cookie;
+    char sUsername[20];
     UT_hash_handle hh;
-} example_user_t;
+}stExample;
 
-int main(int argc,char *argv[]) {
-    int i;
-    example_user_t *user, *users=NULL;
-    
-    /* create elements */
-    for(i=0;i<10;i++) {
-        if ( (user = (example_user_t*)malloc(sizeof(example_user_t))) == NULL) exit(-1);
-        user->id = i;
-        user->cookie = i*i;
-        HASH_ADD_INT(users,id,user);
-    }
+typedef struct _examples {
+    int id;
+    stExample *node;
+    UT_hash_handle hh;
+}stExamples;
 
-    for(user=users; user != NULL; user=(example_user_t*)(user->hh.next)) {
-        printf("user %d, cookie %d\n", user->id, user->cookie);
-    }
-    return 0;
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * 
+ * @param users
+ * @param iUserid
+ * @return 
+ */
+stExample *  fFindItemById(stExample *users,int iUserid){
+      
+    stExample *pFind;
+    HASH_FIND_INT(users,&iUserid,pFind);
+    return pFind;
 }
+
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * 
+ * @param users
+ * @param iUserId
+ * @param sUsername
+ * @return 
+ */
+int fAddItemToHashTable(stExample **users,int iUserId,char *sUsername,stExample **st){
+    int ret = 0;
+
+    HASH_FIND_INT(*users,&iUserId,*st);
+    if (!*st) {
+        *st =  malloc(sizeof(stExample));
+        if (*st == NULL) ret = -1;
+        (*st)->id = iUserId;
+        HASH_ADD_INT(*users,id,*st);
+        strcpy((*st)->sUsername,sUsername);
+        printf("Add an item.\n");
+        ret = 1;
+    } else {
+        printf("Not Uniqueness\n");
+        ret = 0;
+    }
+    return ret;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * 
+ * @param pUsers
+ * @param iUserId
+ * @return 
+ */
+int fDelItemById(stExample *pUsers,int iUserId){
+    
+    int ret = 0;
+    
+    stExample *tmp = fFindItemById(pUsers,iUserId);
+    if (tmp) {
+        HASH_DEL(pUsers,tmp);
+        free(tmp);
+        tmp = NULL;
+        ret = 1;
+    } else {
+        ret = 0;
+    }
+
+    
+    return ret;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * 
+ * @param pUsers
+ */
+void fPrintHashTable(stExample *pUsers){
+    stExample *stFirst;
+    for(stFirst=pUsers; stFirst != NULL; stFirst=(stExample*)(stFirst->hh.next)) {
+        printf("key %d, username %s\n", stFirst->id, stFirst->sUsername);
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+/**
+ * 
+ * @param argc
+ * @param argv
+ * @return 
+ */
+int main(int argc, char** argv) {
+    
+    stExample *pUsers = NULL;
+    stExample *stFirst;
+    int iUserid = 1,ret;
+    char sUsername[] = "Neo";
+    
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    
+    long timestamp = (long)time(NULL)*1000 + tv.tv_usec;
+
+    
+    printf("%ld\n",timestamp);
+    //stFirst = (stExample*)malloc(sizeof(stExample));
+    //int ret = fAddItemToHashTable(pUsers,iUserid,sUsername,stFirst);
+    
+    printf("Program start:\n");
+    
+    //////////////////////////////////////////////////////////////////////////////
+    // add item
+    int i;
+    for (i = 1; i < 20; i++) {
+        ret = fAddItemToHashTable(&pUsers, i, sUsername, &stFirst);
+        if (ret > 0) {
+            printf("use %d find:%s\n", stFirst->id, stFirst->sUsername);
+        } else if (ret == 0) {
+            printf("the key:%d is Not Uniqueness!\n", iUserid);
+        } else {
+            printf("can't create any more memory!\n");
+        }
+    }
+
+    
+
+
+    //////////////////////////////////////////////////////////////////////////////
+    // find item
+    stExample * tmp = fFindItemById(pUsers,iUserid);
+    if(tmp){
+        printf("use key %d find %s\n",tmp->id,tmp->sUsername);
+    }else{
+        printf("use key %d can't find any item!\n",iUserid);
+    }
+    
+    //////////////////////////////////////////////////////////////////////////////
+    // print all
+    fPrintHashTable(pUsers);
+    
+    //////////////////////////////////////////////////////////////////////////////
+    // delete a item
+    ret = fDelItemById(pUsers,iUserid);
+    if (ret > 0) {
+        printf("use key %d delete a item.\n",iUserid);
+    } else {
+        printf("use key %d can't find any item.\n",iUserid);
+    }
+    
+    fPrintHashTable(pUsers);
+
+    
+    
+    
+//    stExamples *pExamples;
+//    stExample *test = malloc(sizeof(stExample));
+//    int ids = 1;
+//    
+//    test->id = 5;
+//    strcpy(test->sUsername,"hello");
+//    HASH_ADD_INT(pExamples,id,test);
+//    
+//    HASH_FIND_INT(pExamples,&ids,test);
+//    
+    return (EXIT_SUCCESS);
+}
+
